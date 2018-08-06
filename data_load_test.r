34ab35de20df83ae79043f006a6c4e6da7ad9056
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2018-08-06 12:04:49 +0100

    Changed variable names (_S* gotten rid of and index numbers at the start)

--- data_load_test.r
+++ data_load_test.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library("DESeq2")
+library(readxl)
 
 gene_tib <- read_tsv("/home/osboxes/Documents/analysis/quant/kallisto.dir/genes.tsv.gz")
 
@@ -8,10 +9,25 @@ transcripts_tib <- read_tsv("/home/osboxes/Documents/analysis/quant/kallisto.dir
 # Looking at data structure
 glimpse(gene_tib)
 summary(gene_tib)
-for(col_name in colnames(gene_tib)){
-  if("WT" in col_name){
-    print(col_name)
-  }
-}
+
+# Remove sample number at the end
+colnames(gene_tib) <- sub("_[^_]+$", "", colnames(gene_tib))
+# Remove index number
+colnames(gene_tib) <-gsub("^[^-]*-","",colnames(gene_tib))
+
+
 # Genes going down, sample name going along
-select
+CFZ_genes <- select(gene_tib, -contains("WT"))
+
+# Separate by time points
+CFZ_6hr <- select(CFZ_genes, -contains("t24"))
+CFZ_24hr <- select(CFZ_genes, -contains("t6"))
+
+# Read in sample sheet
+coldata <- read_excel("sample_sheet.xlsx")
+coldata_6 <- filter(coldata, TimePoint == 6)
+coldata_24 <- filter(coldata, TimePoint == 24)
+
+
+
+
