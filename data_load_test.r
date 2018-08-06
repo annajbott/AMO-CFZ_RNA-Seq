9c481f5f0ea952fab5899ab52be0c1a3e174c884
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2018-08-06 17:55:15 +0100

    Normalised data and heatmap

--- data_load_test.r
+++ data_load_test.r
@@ -1,6 +1,9 @@
 library(tidyverse)
 library("DESeq2")
 library(readxl)
+library("RColorBrewer")
+library(pheatmap)
+
 
 gene_tib <- read_tsv("/home/osboxes/Documents/analysis/quant/kallisto.dir/genes.tsv.gz")
 gene_tib2 <- as.data.frame(gene_tib)[-1]
@@ -77,3 +80,54 @@ dds
 ###################
 ## Pre-filtering ##
 ###################
+# Keep only rows with reads over 10 counts, so that memory of object is reduced
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+dds
+
+# Set the reference (control) level/ factor, against which others will be compared
+dds$Compound <- relevel(dds$Compound, ref = "DMSO")
+
+## Collapse technical replicates??? (Ask adam) ##
+# If you collapse technical replicates will we lose all our variation?
+
+# Contrasting compounds against DMSO, using p adjusted value of 5% as cut off.
+dds_deseq <- DESeq(dds)
+results_26 <- results(dds_deseq, contrast = c("Compound", "26", "DMSO"), alpha = 0.05)
+results_22 <- results(dds_deseq, contrast = c("Compound", "22", "DMSO"), alpha = 0.05)
+results_13 <- results(dds_deseq, contrast = c("Compound", "13", "DMSO"), alpha = 0.05)
+results_18 <- results(dds_deseq, contrast = c("Compound", "18", "DMSO"), alpha = 0.05)
+results_26_p <- results_26[!is.na(results_26$padj) & results_26$padj<= 0.05,]
+results_22_p <- results_22[!is.na(results_22$padj) & results_22$padj<= 0.05,]
+results_13_p <- results_13[!is.na(results_13$padj) & results_13$padj<= 0.05,]
+results_18_p <- results_18[!is.na(results_18$padj) & results_18$padj<= 0.05,]
+
+summary(results_26)
+sum(results_26$padj < 0.05, na.rm=TRUE)
+
+## MLE used here, ask about MAP?
+# Results log2 fold change (LFC) #
+res_26_lfc <- lfcShrink(dds_deseq, coef = 2, res = results_26)
+res_22_lfc <- lfcShrink(dds_deseq, coef = 2, res = results_22)
+
+
+## MA-plot
+plotMA(results_26, ylim = c(-2,2))
+plotMA(res_26_lfc, ylim = c(-2,2))
+
+plotMA(results_22, ylim = c(-2,2))
+plotMA(res_22_lfc, ylim = c(-2,2))
+
+rld <- rlog(dds, blind=FALSE)
+
+plotPCA(rld, intgroup=c("Compound"))
+
+sampleDists <- dist(t(assay(rld)))
+sampleDistMatrix <- as.matrix(sampleDists)
+rownames(sampleDistMatrix) <- rld$Compound
+colnames(sampleDistMatrix) <- NULL
+colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+pheatmap(sampleDistMatrix,
+         clustering_distance_rows=sampleDists,
+         clustering_distance_cols=sampleDists,
+         col=colors)
