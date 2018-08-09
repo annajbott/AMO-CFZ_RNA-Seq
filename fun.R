c724e136f6cd656d3eded6477a59d94e40e55dc7
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2018-08-09 11:03:39 +0100

    function for gene names

--- fun.R
+++ fun.R
@@ -1,10 +1,32 @@
 library(DESeq2)
+library(biomaRt)
 
 results_process <- function(dds, contrast, alpha){
   result_noshrink <- results(dds, contrast = contrast, alpha = alpha)
   result_lfc <- lfcShrink(dds, contrast = contrast, res = result_noshrink)
   # keep only non NA adjusted p values and <0.05
   result_lfc <- result_lfc[!is.na(result_lfc$padj) & result_lfc$padj<= alpha,]
+  # All genes left now are 'statistically significant', so now order by biological significance 
   # Order by largest magnitude fold change
   result_lfc <- result_lfc[rev(order(abs(result_lfc$log2FoldChange))),]
+}
+
+## Fucntion for hashing gene id to name
+gene_id_name <- function(ordered_results, top = 50){
+  ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
+  gene_keys <- getBM(attributes=c('ensembl_gene_id', 'hgnc_symbol'), mart = ensembl)
+  #gene_keys <- gene_keys[!duplicated(gene_keys$ensembl_gene_id),]
+  
+  top_lfc <- ordered_results[1:top,]
+  if(!all(rownames(top_lfc) %in% gene_keys$ensembl_gene_id)){
+    print("not all genes in ensembl db")
+    break
+  }
+  top_lfc$gene_name <- NA
+  for(i in 1:nrow(top_lfc)){
+    gene_id <- rownames(top_lfc)[i]
+    name <- filter(gene_keys, gene_keys$ensembl_gene_id == gene_id)[1,2]
+    top_lfc[i,7] <- name
+  }
+  top_lfc
 }
