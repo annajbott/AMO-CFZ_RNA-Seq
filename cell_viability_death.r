0c9bb03b3e57b6f3e9c03ac242b01a63387fe44b
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2018-08-14 11:43:42 +0100

    Box plots and time analyses for CFZ and WT cells

--- cell_viability_death.r
+++ cell_viability_death.r
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggplot2)
 
 ## Load CSV file ##
 cell_death_data <- read_csv("cell_viability_cell_death.csv")
@@ -8,3 +9,36 @@ cell_death_data <- dplyr::select(cell_death_data, -contains("X"), -contains("Wel
 cell_death_data$Repeat <- as.factor(cell_death_data$Repeat)
 cell_death_data$Compound <- as.factor(cell_death_data$Compound)
 cell_death_data$CellType <- as.factor(cell_death_data$CellType)
+
+cell_death_data <- filter(cell_death_data, Compound != "n/a")
+glimpse(cell_death_data)
+
+# Box plots, see variation
+ggplot(data = cell_death_data, aes(x = Compound, y = FluorescencePrestoBlue, color = Compound)) +
+  geom_boxplot() + 
+  facet_grid(~CellType)
+ggplot(data = cell_death_data, aes(x = Compound, y = FluorescenceCyQUANT, color = Compound)) +
+  geom_boxplot() + 
+  facet_grid(~CellType)
+
+## Presto blue
+## Group by variables, then take mean values
+mean_presto_data <- group_by(cell_death_data, Compound, TimePoint, CellType) %>%
+  summarise(FluorescencePrestoBlue = mean(FluorescencePrestoBlue, na.rm = TRUE))
+
+ggplot(data = mean_presto_data, aes(x = TimePoint, y = FluorescencePrestoBlue, color = Compound)) + 
+  geom_line() +
+  facet_grid(~CellType)
+ungroup(cell_death_data)
+
+
+## CyQuant
+## Group by variables, then take mean values
+mean_cyquant_data <- group_by(cell_death_data, Compound, TimePoint, CellType) %>%
+  summarise(FluorescenceCyQUANT = mean(FluorescenceCyQUANT, na.rm = TRUE))
+
+ggplot(data = mean_cyquant_data, aes(x = TimePoint, y = FluorescenceCyQUANT, color = Compound)) + 
+  geom_line() +
+  geom_point() +
+  facet_grid(~CellType)
+ungroup(cell_death_data)
