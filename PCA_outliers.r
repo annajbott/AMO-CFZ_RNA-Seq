799245e4f0607250702b5ee871ad76c9254c49d1
annajbott <anna.james-bott@st-hildas.ox.ac.uk>
2018-08-17 17:16:09 +0100

    More concise. essData for essential genes in enriched pathway. Unsure why genes are assigned some negative values, genes upregulated?? so can't be fold change.

--- PCA_outliers.r
+++ PCA_outliers.r
@@ -41,38 +41,8 @@ coldata_24 <- coldata[16:28,]
 ## Using DESeq2 Package ##
 ## -------------------- ##
 
-# 6 hour #
-dds <-  DESeqDataSetFromMatrix(countData = CFZ_6hr,
-                               colData = coldata_6,
-                               design = ~ Compound)
-dds
-
-# Prefiltering, removing rows with sum of counts under 10 #
-keep <- rowSums(counts(dds)) >= 10
-dds <- dds[keep,]
-dds
-
-# Set the reference (control) level/ factor, against which others will be compared
-dds$Compound <- relevel(dds$Compound, ref = "DMSO")
-
-dds_deseq <- DESeq(dds)
-
-# 24 hour # 
-dds_24 <-  DESeqDataSetFromMatrix(countData = CFZ_24hr,
-                                  colData = coldata_24,
-                                  design = ~ Compound)
-dds_24
-
-## Pre-filtering ##
-keep <- rowSums(counts(dds_24)) >= 10
-dds_24 <- dds_24[keep,]
-dds_24
-
-# Set the reference (control) level/ factor, against which others will be compared
-dds_24$Compound <- relevel(dds_24$Compound, ref = "DMSO")
-
-# Contrasting compounds against DMSO, using p adjusted value of 5% as cut off.
-dds_deseq24 <- DESeq(dds_24)
+dds_deseq <- dds_get(gene_data = CFZ_6hr, coldata = coldata_6, prefilter = 10, reference = "DMSO")
+dds_deseq24 <- dds_get(gene_data = CFZ_24hr, coldata = coldata_24, prefilter = 10, reference = "DMSO")
 
 ## Regularising transformation ##
 ## --------------------------- ##
@@ -128,16 +98,16 @@ write.csv(genes_data, file = "top50_PC1.csv", row.names = FALSE)
 ## Differential expression analysis without outliers ##
 ## ------------------------------------------------- ##
 
-res_26_lfc <- results_process(dds_deseq, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = TRUE, foldchange_threshold = FALSE )
-res_22_lfc <- results_process(dds_deseq, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = TRUE, foldchange_threshold = FALSE )
-res_13_lfc <- results_process(dds_deseq, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = TRUE, foldchange_threshold = FALSE )
-res_18_lfc <- results_process(dds_deseq, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = TRUE, foldchange_threshold = FALSE )
+res_26_lfc <- results_process(dds_deseq, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_22_lfc <- results_process(dds_deseq, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_13_lfc <- results_process(dds_deseq, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_18_lfc <- results_process(dds_deseq, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
 
 # Taken out a replicate so padj values are using sd with only two replicates, instead set threshold as 1 log2 fold change (double expression)
-res_26_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = 0.75)
-res_22_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = 0.75 )
-res_13_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = 0.75 )
-res_18_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = 0.75 )
+res_26_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE)
+res_22_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE)
+res_13_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_18_lfc_24 <- results_process(dds_deseq24, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
 
 ######################
 ## Pathway Analysis ##
@@ -154,13 +124,37 @@ kg.hsa <- kegg.gsets(species = "hsa", id.type = "kegg", check.new=FALSE)
 kegg.sigmet <- kg.hsa$kg.sets[kg.hsa$sigmet.idx]
 
 # List of results, followed by fold change data frame
-kegg_26_6 <-  kegg_ouput(res_26_lfc, gset = kegg.sigmet, same_dir = TRUE)
-kegg_26_24 <- kegg_ouput(res_26_lfc_24, gset = kegg.sigmet, same_dir = TRUE)
-kegg_13_6 <-  kegg_ouput(res_13_lfc, gset = kegg.sigmet, same_dir = TRUE)
-kegg_13_24 <- kegg_ouput(res_13_lfc_24, gset = kegg.sigmet, same_dir = TRUE)
+kegg_26_6 <- pathway_full(dds = dds_deseq, contrast = c("Compound", "26", "DMSO"), gset = kegg.sigmet,  same_direction = TRUE)
+kegg_26_24 <- pathway_full(dds = dds_deseq24, contrast = c("Compound", "26", "DMSO"), gset = kegg.sigmet,  same_direction = TRUE)
+kegg_13_6 <-  pathway_full(dds = dds_deseq, contrast = c("Compound", "13", "DMSO"), gset = kegg.sigmet,  same_direction = TRUE)
+kegg_13_24 <- pathway_full(dds = dds_deseq24, contrast = c("Compound", "13", "DMSO"), gset = kegg.sigmet,  same_direction = TRUE)
 # Test
 lapply(kegg_13_24[[1]], head)
 
+kegg_26 <- kegg_26_6[[1]]
+fc_kegg_26 <- kegg_26_6[[2]]
+
+
+gs <- unique(unlist(kegg.sigmet[rownames(kegg_26$greater)[1]]))
+fc_kegg_26 <- cbind(fc_kegg_26) # From biostar forum
+essData <- essGene(gs, fc_kegg_26, ref =NULL, samp =NULL)
+convertIDs(rownames(as.data.frame(essData)),"ENTREZID","SYMBOL", org.Hs.eg.db)
+
+# this is for heat map probably won't work with deseq2 format
+head(essData, 4)
+ref1=1:6
+samp1=7:12
+#generated text file for data table, pdf files for heatmap and scatterplot
+for (gs in rownames(gse16873.kegg.p$greater)[1:3]) {
+  outname = gsub(" |:|/", "_", substr(gs, 10, 100))
+  geneData(genes = kegg.gs[[gs]], exprs = essData, ref = ref1,
+           samp = samp1, outname = outname, txt = TRUE, heatmap = TRUE,
+           Colv = FALSE, Rowv = FALSE, dendrogram = "none", limit = 3, scatterplot = TRUE)
+}
+
+
+
+## Pathview crap
 # Get the pathways
 kegg_pathways_26_6 = data.frame(id=rownames(kegg_26_6[[1]]$greater), kegg_26_6[[1]]$less) %>% 
   tbl_df() %>% 
@@ -184,7 +178,3 @@ plot_pathway = function(pid) pathview(gene.data=foldchanges, pathway.id=pid, spe
 # plot multiple pathways (plots saved to disk and returns a throwaway list object)
 tmp = sapply(kegg_ids_13_24, function(pid) pathview(gene.data=kegg_13_24[[2]], pathway.id=pid, species="hsa", out.suffix=out.suffix))
 
-pv.out.list <- sapply(kegg_ids_26_6, function(pid) pathview(gene.data =  kegg_26_6[[2]], pathway.id = pid,species = "hsa", out.suffix=out.suffix))
-
-
-pv.out.list <- sapply(kegg_ids_13_6, function(pid) pathview(gene.data =  kegg_13_6[[2]], pathway.id = pid,species = "hsa", out.suffix=out.suffix))
