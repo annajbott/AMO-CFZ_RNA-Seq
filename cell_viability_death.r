588d9e0d9899b508bcba2403cc1a8f76e4c6caf7
Anna James-Bott <annajames-bott@Annas-MacBook-Air.local>
2018-09-26 13:51:13 +0100

    Whit background. Just CFZ cell viability graph

--- cell_viability_death.r
+++ cell_viability_death.r
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggplot2)
+source("fun.R")
 
 ## Load CSV file ##
 cell_death_data <- read_csv("cell_viability_cell_death.csv")
@@ -9,6 +10,8 @@ cell_death_data <- dplyr::select(cell_death_data, -contains("X"), -contains("Wel
 cell_death_data$Repeat <- as.factor(cell_death_data$Repeat)
 cell_death_data$Compound <- as.factor(cell_death_data$Compound)
 cell_death_data$CellType <- as.factor(cell_death_data$CellType)
+
+medium_data <- filter(cell_death_data, str_detect(SampleName, "medium"))
 
 cell_death_data <- filter(cell_death_data, Compound != "n/a")
 glimpse(cell_death_data)
@@ -42,3 +45,27 @@ ggplot(data = mean_cyquant_data, aes(x = TimePoint, y = FluorescenceCyQUANT, col
   geom_point() +
   facet_grid(~CellType)
 ungroup(cell_death_data)
+
+
+## Just  ##
+cell_death_data_cfz <- filter(cell_death_data, CellType != "WT")
+
+
+max_cells <- filter(cell_death_data_cfz, TimePoint == 6)
+max_cells$TimePoint <- 0
+# zero time equal to average dmso at 6 hrs
+max_cells$FluorescencePrestoBlue <- 190743
+cell_death_data_cfz <- rbind(max_cells,cell_death_data_cfz)
+cell_death_data_cfz <- filter(cell_death_data_cfz, Compound != "DMSO")
+
+
+cell_death_data_cfz_summary <- summarySE(cell_death_data_cfz, measurevar="FluorescencePrestoBlue", groupvars=c("Compound", "TimePoint"))
+
+pd <- position_dodge(0) # move them .05 to the left and right
+plot1 <- ggplot(cell_death_data_cfz_summary, aes(x=TimePoint, y=FluorescencePrestoBlue, colour=Compound))  +
+  geom_errorbar(aes(ymin=FluorescencePrestoBlue-se, ymax=FluorescencePrestoBlue+se), width=3, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd) + xlim(0,50)
+
+plot1 + theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                             panel.grid.minor = element_blank(), axis.line = element_line(colour = "black")) +ylab("Fluorescence") +xlab("Time (hours)")
