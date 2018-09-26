3db5a7b19260111e9c9eb668d29d1c4aedbb3980
Anna James-Bott <annajames-bott@Annas-MacBook-Air.local>
2018-09-26 13:53:38 +0100

    Results for carf

--- wt.r
+++ wt.r
@@ -43,24 +43,52 @@ res_26_lfc_wt <- results_process(dds_deseq_wt, contrast = c("Compound", "26", "D
 res_22_lfc_wt <- results_process(dds_deseq_wt, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
 res_13_lfc_wt <- results_process(dds_deseq_wt, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
 res_18_lfc_wt <- results_process(dds_deseq_wt, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_carf_lfc_wt <- results_process(dds_deseq_wt, contrast = c("Compound", "carf", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+
 
 # Taken out a replicate so padj values are using sd with only two replicates, instead set threshold as 1 log2 fold change (double expression)
 res_26_lfc_24_wt <- results_process(dds_deseq24_wt, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE)
 res_22_lfc_24_wt <- results_process(dds_deseq24_wt, contrast = c("Compound", "22", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE)
 res_13_lfc_24_wt <- results_process(dds_deseq24_wt, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
 res_18_lfc_24_wt <- results_process(dds_deseq24_wt, contrast = c("Compound", "18", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+res_carf_lfc_24_wt <- results_process(dds_deseq24_wt, contrast = c("Compound", "carf", "DMSO"), alpha = 0.05, significant_only = FALSE, foldchange_threshold = FALSE )
+
 
 res_26_6_useful_wt <- res_useful(res_26_lfc_wt, tf = TRUE)
 res_22_6_useful_wt <- res_useful(res_22_lfc_wt, tf = TRUE)
 res_13_6_useful_wt <- res_useful(res_13_lfc_wt, tf = TRUE)
 res_18_6_useful_wt <- res_useful(res_18_lfc_wt, tf = TRUE)
+res_carf_6_useful_wt <- res_useful(res_carf_lfc_wt, tf = NULL)
+
 res_26_24_useful_wt <- res_useful(res_26_lfc_24_wt, tf = TRUE)
 res_22_24_useful_wt <- res_useful(res_22_lfc_24_wt, tf = TRUE)
 res_13_24_useful_wt <- res_useful(res_13_lfc_24_wt, tf = TRUE)
 res_18_24_useful_wt <- res_useful(res_18_lfc_24_wt, tf = TRUE)
+res_carf_24_useful_wt <- res_useful(res_carf_lfc_24_wt, tf = NULL)
+
 
 # Data object so don't have to run the above
-save(res_26_6_useful_wt, res_22_6_useful_wt, res_13_6_useful_wt, res_18_6_useful_wt,res_26_24_useful_wt ,res_22_24_useful_wt , res_13_24_useful_wt , res_18_24_useful_wt, file = "WT_useful_deseq_results.RData")
+save(dds_deseq_wt, dds_deseq24_wt, res_26_6_useful_wt, res_22_6_useful_wt, res_13_6_useful_wt, res_18_6_useful_wt,res_26_24_useful_wt ,res_22_24_useful_wt , res_13_24_useful_wt , res_18_24_useful_wt, file = "WT_useful_deseq_results.RData")
 
-# Can run from here instead
+# Can run from here instead- shortcut
 load("WT_useful_deseq_results.RData")
+
+eTerm_dmso_wt_26_6 <- enricher_analysis(dds_deseq_wt, c("Compound", "26", "DMSO"), ontology = "MsigdbC2REACTOME", result_lfc = res_26_6_useful_wt, alpha = 0.05, foldchange_threshold = FALSE, number_top_genes = 500)
+eTerm_dmso_wt_13_6 <- enricher_analysis(dds_deseq_wt, c("Compound", "13", "DMSO"), ontology = "MsigdbC2REACTOME", result_lfc = res_13_6_useful_wt, alpha = 0.05, foldchange_threshold = FALSE, number_top_genes = 500)
+eTerm_dmso_wt_26_24 <- enricher_analysis(dds_deseq24_wt, c("Compound", "26", "DMSO"), ontology = "MsigdbC2REACTOME", result_lfc = res_26_24_useful_wt, alpha = 0.05, foldchange_threshold = FALSE, number_top_genes = 500)
+eTerm_dmso_wt_13_24 <- enricher_analysis(dds_deseq24_wt, c("Compound", "13", "DMSO"), ontology = "MsigdbC2REACTOME", result_lfc = res_13_24_useful_wt, alpha = 0.05, foldchange_threshold = FALSE, number_top_genes = 500)
+
+
+subnet_26_6_wt <- subneter_analysis(dds_deseq, c("Compound", "26", "DMSO"), result_lfc = res_26_6_useful_wt, alpha = 0.05, number_top_genes = 500, subnet.size = 50)
+xVisNet(g=subnet_26_6_wt, pattern=-log10(as.numeric(V(subnet_26_6_wt)$significance)), colorbar = FALSE, vertex.shape="circle", colormap="yr", signature = FALSE, newpage = FALSE, vertex.label.dist =0.5, vertex.label.cex = 0.6, vertex.label.color = "black")
+#
+subnet_26_24_wt <- subneter_analysis(dds_deseq24, c("Compound", "26", "DMSO"), result_lfc = res_26_24_useful_wt, alpha = 0.05, number_top_genes = 500, subnet.size = 50)
+xVisNet(g=subnet_26_24_wt, pattern=-log10(as.numeric(V(subnet_26_24_wt)$significance)), colorbar = FALSE, vertex.shape="circle", colormap="yr", signature = FALSE, newpage = FALSE, vertex.label.dist =0.7, vertex.label.cex = 0.9, vertex.label.color = "black")
+#
+subnet_13_6_wt <- subneter_analysis(dds_deseq, c("Compound", "13", "DMSO"), result_lfc = res_13_6_useful_wt, alpha = 0.05, number_top_genes = 500,  subnet.size = 50)
+xVisNet(g=subnet_13_6_wt, pattern=-log10(as.numeric(V(subnet_13_6_wt)$significance)), colorbar = FALSE, vertex.shape="circle", colormap="yr", signature = FALSE, newpage = FALSE, vertex.label.dist =0.7, vertex.label.cex = 0.9, vertex.label.color = "black")
+#
+subnet_13_24_wt <- subneter_analysis(dds_deseq24, c("Compound", "13", "DMSO"), result_lfc = res_13_24_useful_wt, alpha = 0.05, number_top_genes = 500, subnet.size = 50)
+xVisNet(g=subnet_13_24_wt, pattern=-log10(as.numeric(V(subnet_13_24_wt)$significance)), colorbar = FALSE, vertex.shape="circle", colormap="yr", signature = FALSE, newpage = FALSE, vertex.label.dist =0.7, vertex.label.cex = 0.9, vertex.label.color = "black")
+#
+
