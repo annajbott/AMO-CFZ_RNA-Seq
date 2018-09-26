cdc7398bd52de9641fbadbf8d37fba17efbd81cf
Anna James-Bott <annajames-bott@Annas-MacBook-Air.local>
2018-09-26 13:51:56 +0100

    Updated graphs. ARS bar plot. fixed bug for 6 vs 24 hr plot

--- deseq2_analysis.r
+++ deseq2_analysis.r
@@ -202,40 +202,132 @@ pheatmap(sampleDistMatrix,
 ## Plots of AAR known genes ##
 ## ------------------------ ##
 
-samples_list <- list(genes_dif_expressed_13_6, genes_dif_expressed_13_24, genes_dif_expressed_26_6, genes_dif_expressed_26_24)
-gene_list <- c("EIF4EBP1", "ASNS", "GPT2", "DDIT3" , "ATF4", "SLC6A9")
+samples_list <- list(res_13_6_useful, res_13_24_useful, res_26_6_useful, res_26_24_useful,res_18_6_useful, res_18_24_useful, res_22_6_useful, res_22_24_useful)
+gene_list <- c("EIF4EBP1", "ASNS", "GPT2", "DDIT3" , "ATF4", "SLC6A9", "TRIB3", "ATF3")
 plot_table <- data.frame()
 
 plot_table[1:2,1] <- "13"
 plot_table[3:4,1] <- "26"
-plot_table[c(1,3),2] <- "6"
-plot_table[c(2,4),2] <- "24"
-rownames(plot_table) <- c("13_6", "13_24", "26_6", "26_24")
+plot_table[5:6,1] <- "18"
+plot_table[7:8,1] <- "22"
+plot_table[seq(1,7, by =2),2] <- "6"
+plot_table[seq(2,8, by =2),2] <- "24"
+rownames(plot_table) <- c("13_6", "13_24", "26_6", "26_24", "18_6", "18_24","22_6","22_24")
 colnames(plot_table) <- c("Compound", "TimePoint")
 
 for(i in seq_along(samples_list)){
   for(j in seq_along(gene_list)){
-    subset_data <- samples_list[[i]] %>% filter(str_detect(hgnc_symbol, gene_list[j]))
+    subset_data <- samples_list[[i]] %>% filter(str_detect(gene_name, gene_list[j]))
     plot_table[i,j + 2] <- subset_data[1,"log2FoldChange"]
   }
 }
 colnames(plot_table) <- c("Compound", "TimePoint",gene_list)
 plot_table$Compound <- as.factor(plot_table$Compound)
+plot_table$Compound <- factor(plot_table$Compound, levels = c("13","26","18","22"))
+
 plot_table$TimePoint <- as.factor(plot_table$TimePoint)
 
 plot_table_tidy <- gather(plot_table,"gene", "Log2FoldChange", -c(Compound, TimePoint))
 
-ggplot(data=plot_table_tidy, aes(x=gene, y=Log2FoldChange, fill = Compound)) +
-  geom_bar(stat = "summary", fun.y = "mean", position=position_dodge())
+plotaar <- ggplot(data=plot_table_tidy, aes(x=gene, y=Log2FoldChange, fill = Compound)) +
+  geom_bar(stat = "summary", fun.y = "mean", position=position_dodge()) + xlab("Gene") +ylab("Log 2 Fold Change")
+
+plotaar +  theme_bw()
+## Plots of ARS genes ##
+## ------------------ ##
+
+samples_list <- list(res_26_6_useful, res_26_24_useful, res_13_6_useful, res_13_24_useful)
+gene_list <- c("WARS", "CARS", "SARS", "TARS" , "GARS", "RARS", "NARS", "LARS", "AARS", "YARS" , "EPRS", "MARS", "FARSB", "IARS", "HARS", "VARS" , "FARSA", "KARS", "DARS", "QARS")
+plot_table <- data.frame()
+
+plot_table[1:2,1] <- "26"
+plot_table[3:4,1] <- "13"
+plot_table[c(1,3),2] <- "6"
+plot_table[c(2,4),2] <- "24"
+rownames(plot_table) <- c("13_6", "13_24", "26_6", "26_24")
+colnames(plot_table) <- c("Compound", "TimePoint")
+plot_table_padj <- plot_table
+
+for(i in seq_along(samples_list)){
+  for(j in seq_along(gene_list)){
+    subset_data <- samples_list[[i]] %>% filter(str_detect(gene_name, gene_list[j]))
+    plot_table[i,j + 2] <- subset_data[1,"log2FoldChange"]
+    plot_table_padj[i, j +2] <- subset_data[1,"padj"]
+  }
+}
+colnames(plot_table) <- c("Compound", "TimePoint",gene_list)
+colnames(plot_table_padj) <- c("Compound", "TimePoint",gene_list)
+plot_table$Compound <- as.factor(plot_table$Compound)
+plot_table$TimePoint <- as.factor(plot_table$TimePoint)
+plot_table_padj$Compound <- as.factor(plot_table_padj$Compound)
+plot_table_padj$TimePoint <- as.factor(plot_table_padj$TimePoint)
+
+plot_table_tidy <- gather(plot_table,"gene", "log2fc", -c(Compound, TimePoint))
+plot_table_padjtidy <- gather(plot_table_padj,"gene", "padj", -c(Compound, TimePoint))
+plot_table_padjtidy$gene <- as.factor(plot_table_tidy$gene)
+
+# Useful for marking which are significant 
+plot_table_padjtidy_2 <- plot_table_padjtidy[,-2]
+plot_table_padjtidy_2 <- aggregate(x = plot_table_padjtidy_2, by = list(gene = plot_table_padjtidy_2$gene, comp = plot_table_padjtidy_2$Compound), FUN = mean)
+plot_table_padjtidy_2$sig <- ifelse(plot_table_padjtidy_2[,5] < 0.05, TRUE, FALSE)
+
+ARS1 <- ggplot(data=plot_table_tidy, aes(x=gene, y=log2fc, fill = Compound, color = )) +
+  geom_bar(stat = "summary", fun.y = "mean", position=position_dodge())  + xlab("ARS Gene") + ylab("Log2 Fold Change") 
+
+ARS1 +  theme_bw() + theme(axis.text.x = element_text(angle = 90))
+
+## Plots of mitochondrial ARS genes ##
+## -------------------------------- ##
+
+samples_list <- list(res_26_6_useful, res_26_24_useful, res_13_6_useful, res_13_24_useful)
+gene_list <- c("TARS2", "NARS2", "EARS2" , "WARS2", "RARS2", "MARS2", "YARS2", "SARS2", "IARS2" , "HARS2", "VARS2", "PARS2", "LARS2", "FARS2", "DARS2" , "AARS2", "CARS2")
+plot_table <- data.frame()
+
+plot_table[1:2,1] <- "26"
+plot_table[3:4,1] <- "13"
+plot_table[c(1,3),2] <- "6"
+plot_table[c(2,4),2] <- "24"
+rownames(plot_table) <- c("13_6", "13_24", "26_6", "26_24")
+colnames(plot_table) <- c("Compound", "TimePoint")
+plot_table_padj <- plot_table
+
+for(i in seq_along(samples_list)){
+  for(j in seq_along(gene_list)){
+    subset_data <- samples_list[[i]] %>% filter(str_detect(gene_name, gene_list[j]))
+    plot_table[i,j + 2] <- subset_data[1,"log2FoldChange"]
+    plot_table_padj[i, j +2] <- subset_data[1,"padj"]
+  }
+}
+colnames(plot_table) <- c("Compound", "TimePoint",gene_list)
+colnames(plot_table_padj) <- c("Compound", "TimePoint",gene_list)
+plot_table$Compound <- as.factor(plot_table$Compound)
+plot_table$TimePoint <- as.factor(plot_table$TimePoint)
+plot_table_padj$Compound <- as.factor(plot_table_padj$Compound)
+plot_table_padj$TimePoint <- as.factor(plot_table_padj$TimePoint)
+
+plot_table_tidy <- gather(plot_table,"gene", "log2fc", -c(Compound, TimePoint))
+plot_table_padjtidy <- gather(plot_table_padj,"gene", "padj", -c(Compound, TimePoint))
+plot_table_padjtidy$gene <- as.factor(plot_table_tidy$gene)
+
+# Useful for marking which are significant 
+plot_table_padjtidy_2 <- plot_table_padjtidy[,-2]
+plot_table_padjtidy_2 <- aggregate(x = plot_table_padjtidy_2, by = list(gene = plot_table_padjtidy_2$gene, comp = plot_table_padjtidy_2$Compound), FUN = mean)
+plot_table_padjtidy_2$sig <- ifelse(plot_table_padjtidy_2[,5] < 0.05, TRUE, FALSE)
+
+ARS2 <- ggplot(data=plot_table_tidy, aes(x=gene, y=log2fc, fill = Compound, color = )) +
+  geom_bar(stat = "summary", fun.y = "mean", position=position_dodge()) + xlab("ARS Gene") + ylab("Log2 Fold Change") + ylim(-0.52,2.1)
+
+
+ARS2 +  theme_bw() + theme(axis.text.x = element_text(angle = 90))
 
 ## 6hr vs 24 hr plots ##
 ## ------------------ ##
 
 ## Plot of gene log fold change 6 hr vs 24 hr ##
-all_results_26_6 <- results_process(dds_deseq, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE)
-all_results_26_24 <- results_process(dds_deseq24_o, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE)
-all_results_13_6 <- results_process(dds_deseq, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE)
-all_results_13_24 <- results_process(dds_deseq24_o, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE)
+all_results_26_6 <- results_process(dds_deseq, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, order = FALSE)
+all_results_26_24 <- results_process(dds_deseq24_o, contrast = c("Compound", "26", "DMSO"), alpha = 0.05, significant_only = FALSE, order = FALSE)
+all_results_13_6 <- results_process(dds_deseq, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, order = FALSE)
+all_results_13_24 <- results_process(dds_deseq24_o, contrast = c("Compound", "13", "DMSO"), alpha = 0.05, significant_only = FALSE, order = FALSE)
 
 # Get rid of NAs and blank p values. Make match
 #26
@@ -264,12 +356,13 @@ time_vs_26 <- as.data.frame(cbind(kept$log2FoldChange, kept2$log2FoldChange))
 rownames(time_vs_26) <- rownames(kept)
 colnames(time_vs_26) <- c("log_fold_change_6hr", "log_fold_change_24hr")
 time_vs_26$threshold_status <- ifelse(abs(time_vs_26$log_fold_change_6hr) >= 1 & abs(time_vs_26$log_fold_change_24hr) >= 1, TRUE, FALSE)
-ggplot(time_vs_26, aes(x=log_fold_change_6hr, y=log_fold_change_24hr, color = threshold_status)) + geom_point() +
+plot1 <- ggplot(time_vs_26, aes(x=log_fold_change_6hr, y=log_fold_change_24hr, color = threshold_status)) + geom_point() +
  labs(x = "Log 2 Fold Change (NCP 26 vs DMSO)- 6hr", y =  "Log 2 Fold Change (NCP 26 vs DMSO)- 24hr", color = "Exceed threshold" ) +
   scale_colour_manual(values = c("Black", "Red")) +
   geom_abline(intercept = 0, slope = 1) +
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 0)
+plot1 + theme(panel.background = element_blank()) 
 
 
 # 13 data frame and plot
@@ -278,10 +371,12 @@ rownames(time_vs_13) <- rownames(kept_13)
 colnames(time_vs_13) <- c("log_fold_change_6hr", "log_fold_change_24hr")
 # New column indicating if gene expression has doubled for both 6hr and 24 hr, can then colour graph using this variable
 time_vs_13$threshold_status <- ifelse(abs(time_vs_13$log_fold_change_6hr) >= 1 & abs(time_vs_13$log_fold_change_24hr) >= 1, TRUE, FALSE )
-ggplot(time_vs_13, aes(x=log_fold_change_6hr, y=log_fold_change_24hr, color = threshold_status)) + 
+plot2 <- ggplot(time_vs_13, aes(x=log_fold_change_6hr, y=log_fold_change_24hr, color = threshold_status)) + 
   geom_point() + 
   labs(x = "Log 2 Fold Change (MAZ 13 vs DMSO)- 6hr", y =  "Log 2 Fold Change (MAZ 13 vs DMSO)- 24hr", color = "Exceed threshold" ) +
   scale_colour_manual(values = c("Black", "Red")) +
   geom_abline(intercept = 0, slope = 1) +
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 0)
+
+plot2 + theme(panel.background = element_blank()) 
